be6c1bdbdb46c8752030835847be5fa6057b7fbe
Tebogo <tebogomakhubu1@gmail.com>
2025-04-22 17:22:30 +0200

    updated main.tf file

--- terraform_setup/main.tf
+++ terraform_setup/main.tf
@@ -1,41 +1,31 @@
-
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.30.0"
     }
   }
 }
 
 provider "google" {
-   project = "your gcp project id"
-   region  = "africa-south1"
+  project = "<Your Project ID>"
+  region  = "europe-west10"
 }
 
-resource "google_storage_bucket" "data-lake-bucket"{
-    name = "your own unique name for your storage bucket"
-    location = "AFR"
-    storage_class = "STANDARD"
-    uniform_bucket_level_access = true
+resource "google_storage_bucket" "data-lake-bucket" {
+  name                        = "redp_zillow_project_datalake"
+  location                    = "EU"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
 
- versioning {
+  versioning {
     enabled = true
+  }
 }
 
- lifecycle_rule {
-    action{
-        type = "Delete"
-    }
-    condition{
-        age = 30 // days
-    }
-}
-force_destroy = true
-}
 
-resource "google_bigquery_dataset" "datset" {
-    dataset_id = "your own unique name for your bigquery dataset"
-    project = "your gcp project id"
-    location = "AFR"
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "redp_zillow_project_dataset"
+  project    = "<Your Project ID>"
+  location   = "EU"
 }
